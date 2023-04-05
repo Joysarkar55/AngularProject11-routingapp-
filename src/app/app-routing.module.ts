0d7672dc18ea191a9e0a7646408f3d3e553b8307
JS952001 <sarkarpritam871@gmail.com>
2023-04-05 14:27:31 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
+import {NotfoundComponent} from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", pathMatch:"full", redirectTo:"/home"},
+{path: 'order', component: OrderComponent},
+{path: 'home', component: HomeComponent},
+{path: '**', component: NotfoundComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
